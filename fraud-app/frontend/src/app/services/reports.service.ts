da268f9eb4020043f92c9e63fcb935c4e7d3beae
Joncross202 <cro21035@byui.edu>
2024-12-16 10:58:36 -0500

    testing DNS name

--- fraud-app/frontend/src/app/services/reports.service.ts
+++ fraud-app/frontend/src/app/services/reports.service.ts
@@ -6,8 +6,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class ReportsService {
-  private apiUrl = 'https://PangolinLoadBalancer-943882191.us-east-1.elb.amazonaws.com/reports'; // Base URL for reports
-  private annotationsUrl = 'https://PangolinLoadBalancer-943882191.us-east-1.elb.amazonaws.com/annotations'; // Base URL for annotations
+  private apiUrl = 'https://api.skillsolving.ai/reports'; // Base URL for reports
+  private annotationsUrl = 'https://api.skillsolving.ai/annotations'; // Base URL for annotations
 
   constructor(private http: HttpClient) { }
 
